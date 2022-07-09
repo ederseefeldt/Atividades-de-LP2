3f0d37df1b6c833aa5eed658fb6777488853047e
Eder Seefeldt <ederseefeldt2002@gmail.com>
2022-07-09 18:45:51 -0300

    Exe 01

---
+++ Exe 01/script.js
@@ -0,0 +1,9 @@
+function acende() {
+    let NO = document.getElementById("span");
+    NO.setAttribute("class", "")
+}
+
+function desliga() {
+    let NO = document.getElementById("span");
+    NO.setAttribute("class", "vagas");
+}
